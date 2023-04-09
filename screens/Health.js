c39c5a73b2189d540ced38f07d2cf83d3a097517
developertanvir2019 <developertanvir2019@gmail.com>
2023-04-10 00:50:41 +0600

    all finish

--- screens/Health.js
+++ screens/Health.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Text } from 'react-native';
 import { View } from 'react-native';
 
 export default function Health() {
     return (
         <View>
-
+            <Text>Health page</Text>
         </View>
     )
 }
--- screens/Sports.js
+++ screens/Sports.js
@@ -1,9 +1,10 @@
+import { Text } from 'native-base';
 import React from 'react'
 import { View } from 'react-native';
 export default function Sports() {
     return (
         <View>
-
+            <Text>sports page</Text>
         </View>
     )
 }
